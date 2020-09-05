b7682b490bef6230138ee79acc280825f4da9ee4
PESchoenberg <pablo.edronkin@yahoo.com>
2020-09-05 13:27:10 -0300

    New functions.

--- grsp6.scm
+++ grsp6.scm
@@ -31,26 +31,31 @@
   #:use-module (grsp grsp3)
   #:use-module (grsp grsp4)
   #:use-module (grsp grsp5)  
-  #:export (grsp-ds))
+  #:export (grsp-ds
+	    grsp-lorentz-factor
+	    grsp-time-dilation
+	    grsp-length-contraction
+	    grsp-lorentz-transf-x))
 
 
-;; grsp-ds - Calculate intervals in Euclidean space or Minkowski spacetime.
+;; grsp-ds - Calculate intervals in Euclidean space or Minkowski spacetime (norm
+;; of a time-like vector).
 ;;
 ;; Arguments:
+;; - p_t1: t1.
+;; - p_t2: t2.
 ;; - p_x1: x1.
 ;; - p_x2: x2.
 ;; - p_y1: y1.
 ;; - p_y2: y2.
 ;; - p_z1: z1.
 ;; - p_z2: z2.
-;; - p_t1: t1.
-;; - p_t2: t2.
 ;;
 ;; Sources:
 ;; - En.wikipedia.org. 2020. Minkowski Space. [online] Available at:
 ;;   https://en.wikipedia.org/wiki/Minkowski_space [Accessed 31 August 2020].
 ;;
-(define (grsp-ds p_x1 p_x2 p_y1 p_y2 p_z1 p_z2 p_t1 p_t2)
+(define (grsp-ds p_t1 p_t2 p_x1 p_x2 p_y1 p_y2 p_z1 p_z2)
   (let ((res1 0)
 	(nx 0)
 	(ny 0)
@@ -66,24 +71,116 @@
 		  (set! l #f)
 		  (set! nf 1)))))
 
-    ;; x
+    ;; x displacement.
     (set! nx (* (grsp-osbv "#-" 2 p_x1 p_x2) nf))
 
-    ;; y
+    ;; y displacement.
     (set! ny (* (grsp-osbv "#-" 2 p_y1 p_y2) nf))
 
-    ;; z
+    ;; z displacement.
     (set! nz (* (grsp-osbv "#-" 2 p_z1 p_z2) nf))
 
-    ;; t
+    ;; t displacement.
     (cond ((equal? l #t)
 	   (set! nt (expt (* (gconst "c") (- p_t1 p_t2)) 2 ))))
 
     ;; Calculate root of summation of dimensions. If Euclidean space is
     ;; used, nf = 1 so that spacelike vectors will be positive (+ + +), and if
     ;; spacetime is used, nf = -1 so that spacelike vectors become negative and
-    ;; the timelike vector is trated as positive (- - - +).
+    ;; the timelike vector is trated as positive (+ - - -).
     (set! res1 (sqrt (+ (+ (+ nx ny) nz) nt)))
 	   
+    res1))
+
+
+;; grsp-lorentz-factor - Calculates gamma, or Lorentz factor.
+;;
+;; Arguments: 
+;; - p_v1: relative velocity between inertial frames (should be p_v1 < c).
+;;
+;; Sources:
+;; - https://en.wikipedia.org/wiki/Lorentz_factor
+;; - En.wikipedia.org. 2020. List Of Relativistic Equations. [online] Available
+;;   at: https://en.wikipedia.org/wiki/List_of_relativistic_equations
+;;   [Accessed 4 September 2020].
+;;
+(define (grsp-lorentz-factor p_v1)
+  (let ((res1 0))
+
+    (set! res1 (/ 1 (sqrt (- 1 (/ (expt p_v1 2) (expt (gconst "c") 2))))))
+
+    res1))
+
+
+;; grsp-time-dilation - Calculates time t' from t at a given position x in
+;; same inertial frame
+;;
+;; Arguments: 
+;; - p_v1: relative velocity.
+;; - p_t1: proper time.
+;;
+;; Sources:
+;; - En.wikipedia.org. 2020. List Of Relativistic Equations. [online] Available
+;;   at: https://en.wikipedia.org/wiki/List_of_relativistic_equations
+;;   [Accessed 4 September 2020].
+;;
+(define (grsp-time-dilation p_v1 p_t1)
+  (let ((res1 0))
+
+    (set! res1 (* (grsp-lorentz-factor p_v1) p_t1))
+
+    res1))
+
+
+;; grsp-length-contraction - Calculates length contraction under relativistic
+;; conditions.
+;;
+;; Arguments: 
+;; - p_v1: relative velocity.
+;; - p_d1: proper length.
+;;
+;; Sources:
+;; - En.wikipedia.org. 2020. List Of Relativistic Equations. [online] Available
+;;   at: https://en.wikipedia.org/wiki/List_of_relativistic_equations
+;;   [Accessed 4 September 2020].
+;;
+(define (grsp-length-contraction p_v1 p_d1)
+  (let ((res1 0))
+
+    (set! res1 (/ p_d1 (grsp-lorentz-factor p_v1)))
+
+    res1))
+
+
+;; grsp-lorentz-transf-x - Lorentz transformations (x).
+;;
+;; Arguments: 
+;; - p_v1: relative velocity.
+;; - p_t1: proper time.
+;; - p_x1: x coord.
+;; - p_y2: y coord.
+;; - p_z1: z coord.
+;;
+;; Sources:
+;; - En.wikipedia.org. 2020. List Of Relativistic Equations. [online] Available
+;;   at: https://en.wikipedia.org/wiki/List_of_relativistic_equations
+;;   [Accessed 4 September 2020].
+;;
+(define (grsp-lorentz-transf-x p_v1 p_t1 p_x1 p_y1 p_z1)
+  (let ((res1 '())
+	(t2 0)
+	(x2 0)
+	(y2 0)
+	(z2 0)
+	(l1 0))
+
+    (set! l1 (grsp-lorentz-factor p_v1))
+    (set! t2 (* l1 (- p_t1 (/ (* p_v1 p_x1) (expt (gconst "c") 2)))))
+    (set! x2 (* l1 (- p_x1 (* p_v1 p_t1))))
+    (set! y2 p_y1)
+    (set! z2 p_z1)
+    
+    (set! res1 (list t2 x2 y2 z2))
+
     res1))
 
