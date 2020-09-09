7cb534693e4f96e2fd1f84cad0461ae687b7c460
PESchoenberg <pablo.edronkin@yahoo.com>
2020-09-09 17:05:18 -0300

    New functions.

--- grsp6.scm
+++ grsp6.scm
@@ -35,7 +35,14 @@
 	    grsp-lorentz-factor
 	    grsp-time-dilation
 	    grsp-length-contraction
-	    grsp-lorentz-transf-x))
+	    grsp-lorentz-transf-x
+	    grsp-beta
+	    grsp-velocity
+	    grsp-acceleration
+	    grsp-frequency-observed
+	    grsp-frequency-emitted
+	    grsp-frequency-osrel
+	    grsp-redshift))
 
 
 ;; grsp-ds - Calculate intervals in Euclidean space or Minkowski spacetime (norm
@@ -107,8 +114,9 @@
 (define (grsp-lorentz-factor p_v1)
   (let ((res1 0))
 
-    (set! res1 (/ 1 (sqrt (- 1 (/ (expt p_v1 2) (expt (gconst "c") 2))))))
-
+    ;; (set! res1 (/ 1 (sqrt (- 1 (/ (expt p_v1 2) (expt (gconst "c") 2))))))
+    (set! res1 (/ 1 (sqrt (- 1 (expt (grsp-beta p_v1) 2)))))
+    
     res1))
 
 
@@ -183,4 +191,160 @@
     (set! res1 (list t2 x2 y2 z2))
 
     res1))
+
+
+;; grsp-beta - Calculates the beta term of the Lortentz factor.
+;;
+;; Arguments:
+;; - p_v1: relative velocity between two intertial frames.
+;;
+;; Sources:
+;; - En.wikipedia.org. 2020. List Of Relativistic Equations. [online] Available
+;;   at: https://en.wikipedia.org/wiki/List_of_relativistic_equations
+;;   [Accessed 4 September 2020].
+;;
+(define (grsp-beta p_v1)
+  (let ((res1 0))
+
+    (set! res1 (/ p_v1 (gconst "c")))
+
+    res1))
+
+
+;; grsp-velocity - Calculates velocity on one axis.
+;;
+;; Arguments:
+;; - p_x1: coord 1.
+;; - p_x2: coord 2.
+;; - p_t1: time 1.
+;; - p_t2: time 2.
+;;
+(define (grsp-velocity p_x1 p_x2 p_t1 p_t2)
+  (let ((res1 0))
+
+    (set! res1 (/ (- p_x2 p_x1) (- p_t2 p_t1)))
+
+    res1))
+
+
+;; grsp-acceleration - Calculates acceleration on one axis.
+;;
+;; Arguments:
+;; - p_v1: velocity 1.
+;; - p_v2: velocity 2.
+;; - p_t1: time 1.
+;; - p_t2: time 2.
+;;
+(define (grsp-acceleration p_v1 p_v2 p_t1 p_t2)
+  (let ((res1 0))
+
+    (set! res1 (/ (- p_v2 p_v1) (- p_t2 p_t1)))
+
+    res1))    
+
+
+;; grsp-frequency-observed - Calculates the observed frequency.
+;;
+;; Arguments:
+;; - p_s1: see grsp-frequency-osrel.
+;; - p_v1: see grsp-frequency-osrel.
+;; - p_v2: see grsp-frequency-osrel.
+;; - p_v3: see grsp-frequency-osrel.
+;; - p_f1: emitted frequency
+;;
+;; Sources:
+;; - En.wikipedia.org. 2020. Doppler Effect. [online] Available at:
+;;   https://en.wikipedia.org/wiki/Doppler_effect [Accessed 7 September 2020].
+;;
+(define (grsp-frequency-observed p_s1 p_v1 p_v2 p_v3 p_f1)
+  (let ((res1 0))
+    
+    (set! res1 (* p_f1 (grsp-frequency-osrel p_s1 p_v1 p_v2 p_v3)))
+
+    res1))
+
+
+;; grsp-frequency-emitted - Calculates the emitted frequency.
+;;
+;; Arguments:
+;; - p_s1: see grsp-frequency-osrel.
+;; - p_v1: see grsp-frequency-osrel.
+;; - p_v2: see grsp-frequency-osrel.
+;; - p_v3: see grsp-frequency-osrel.
+;; - p_f2: observed frequency
+;;
+;; Sources:
+;; - En.wikipedia.org. 2020. Doppler Effect. [online] Available at:
+;;   https://en.wikipedia.org/wiki/Doppler_effect [Accessed 7 September 2020].
+;;
+(define (grsp-frequency-emitted p_s1 p_v1 p_v2 p_v3 p_f2)
+  (let ((res1 0))
+
+    (set! res1 (/ p_f2 (grsp-frequency-osrel p_s1 p_v1 p_v2 p_v3)))
+
+    res1))
+
+;; grsp-frequency-osrel - Observer - source relationship.
+;;
+;; Arguments:
+;; - p_s1: string, relative movement between the source and the receiver.
+;;   - "#++": p_v1 is added to p_v2 (receiver moves toward source), and p_v1 is
+;;     added to p_v2 (source moves away from the receiver).
+;;   - "$+-": p_v1 is added to p_v2 (receiver moves toward source), and p_v3 is
+;;     substracted from p_v1 (source is moving towards the receiver).
+;;   - "$-+": p_v2 is substracted from p_v1 (receiver moves away from source),
+;;     and p_v1 is added to p_v2 (source moves away from the receiver).
+;;   - "$--": p_v2 is substracted from p_v1 (receiver moves away from source),
+;;     and p_v3 is substracted from p_v1 (source is moving towards the receiver).
+;; - p_v1: propagation speed in the medium.
+;; - p_v2: speed of the receiver.
+;; - p_v3: speed of the source.
+;;
+;; Sources:
+;; - En.wikipedia.org. 2020. Doppler Effect. [online] Available at:
+;;   https://en.wikipedia.org/wiki/Doppler_effect [Accessed 7 September 2020].
+;;
+(define (grsp-frequency-osrel p_s1 p_v1 p_v2 p_v3)
+  (let ((res1 0)
+	(n1 1)
+	(n2 1))
+
+    ;; Conditions of source and receiver.
+    (cond ((equal? p_s1 "#+-")
+	   (set! n1 -1))
+	  ((equal? p_s1 "#-+")
+	   (set! n2 -1))
+	  ((equal? p_s1 "#--")
+	   (set! n1 -1)
+	   (set! n2 -1)))
+    
+    (set! res1 (/ (+ p_v1 (* n1 p_v2)) (+ p_v1 (* n2 p_v3))))
+
+    res1))
+	
+
+;; grsp-redshift - Calculates redshift based on wavelength or frequency.
+;;
+;; Arguments:
+;; - p_s1: string. Defines if wl or fr is used.
+;;   - "#wl": for wavelength.
+;;   - "#fr": for frequency.
+;; - p_n1: value that corresponds to the emitter (wl or fr).
+;; - p_n2: value that corresponds to the observer (wl or fr).
+;;
+;; Sources:
+;; - En.wikipedia.org. 2020. Redshift. [online] Available at:
+;;   https://en.wikipedia.org/wiki/Redshift [Accessed 9 September 2020].
+;;
+(define (grsp-redshift p_s1 p_n1 p_n2)
+  (let ((res1 0))
+
+    (cond ((equal? p_s1 "#fr")
+	   (set! res1 (/ (- p_n1 p_n2) p_n2)))
+	  ((equal? p_s1 "#wl")
+	   (set! res1 (/ (- p_n2 p_n1) p_n1))))
+
+    res1))
+
+
 
